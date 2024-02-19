9d91d195f42a564885e9e5903d1fbf8aca170b69
Adriana Bacilio <ing.adrianafloresbac@gmail.com>
2024-02-19 13:30:37 -0600

    ci: funcion pedir nombre

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hola CH38"); 
+
+function enviarSaludo() {
+    alert("Bienvenido :)");
+}
+
+
+/**
+ * Pedir el nombre de la persona usando prompt ()
+ * Mostrar saludo y nombre de la persona con alert()
+ */
+
+
+function enviarSaludoAPersona() {
+    // Pedir al usuario que ingrese su nombre
+    var nombre = prompt("Por favor, ingresa tu nombre:");
+
+    // Comprobar si se ingresó un nombre
+    if (nombre != null && nombre != "") {
+        // Mostrar el saludo con el nombre ingresado
+        alert("¡Hola, " + nombre + "! Bienvenido.");
+    } else {
+        // Mostrar un mensaje si no se ingresó un nombre
+        alert("No ingresaste ningún nombre. Por favor, intenta de nuevo.");
+    }
+}
